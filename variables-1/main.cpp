60cf682a238941ead0bff30487ee78b578244d7e
Cornelius Kasokola <41131476+Cornelius98@users.noreply.github.com>
2026-06-24 11:32:46 +0200

    Update

---
+++ variables-1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Short
+    short my_short = 50;
+    cout << "Short: " << my_short << endl;
+
+    //int
+    int my_int = 3000000;
+    cout << "Int: " << my_int << endl;
+
+    //long
+    long my_long = 500000;
+    cout << "My long: " << my_long << endl;
+
+    //Unsigned int (positive number)
+    unsigned int my_unsigned_int = 500;
+    cout << "Unsigned int: " << my_unsigned_int << endl;
+
+    //Signed means negative signed number
+    signed short my_signed_short = -20;
+    cout << "Signed short: " << my_signed_short << endl;
+
+    long long int my_long_long_int = 10000000000000000000000000000000000000000000000000;
+    cout << "My long long int: " << my_long_long_int << endl;
+
+    //float
+    float my_float = 23.5;
+    cout << "My float: " << my_float << endl;
+
+    //double
+    double my_double =2.564444989;
+    cout << "My double: " << my_double << endl;
+
+    //Char
+    char my_char = 'a';
+    cout << "My char: " << my_char << endl;
+
+    //String
+    string my_string = "abc";
+    cout << "String: " << my_string << endl;
+
+    //Boolean
+    bool my_bool =true;
+    cout << "My Bool: " << my_bool << endl;
+
+    bool my_boolean =false;
+    cout << "My boolean: " << my_boolean << endl;
+    return 0;
+}
